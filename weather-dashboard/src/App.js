9eb0982d26031e6365cedb7637e14d1282e7579b
Bridget Shannon <bridgetrshannon@gmail.com>
2020-12-16 14:33:34 -0500

    added api key and base url from openweathermap

--- weather-dashboard/src/App.js
+++ weather-dashboard/src/App.js
@@ -1,4 +1,8 @@
-import "./App.css";
+import React from "react";
+const api = {
+  key: "c1d5301af636b9b6f1c8a7606fd79214",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return <div className="App">Hello</div>;
